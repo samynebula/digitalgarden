2973e96ef4db55e53a5d71d1d147520c18bb1c58
samynebula <sam.zandiani@gmail.com>
2024-11-13 17:59:40 +0330

    Quartz sync: Nov 13, 2024, 5:59 PM

--- quartz/components/ArticleTitle.tsx
+++ quartz/components/ArticleTitle.tsx
@@ -3,10 +3,8 @@ import { classNames } from "../util/lang"
 
 const ArticleTitle: QuartzComponent = ({ fileData, displayClass }: QuartzComponentProps) => {
   const title = fileData.frontmatter?.title
-  if (title) {
-    return <h1 class={classNames(displayClass, "article-title")}>{title}</h1>
-  } else {
-    return null
+  if (fileData.slug === "index") {
+    return <></>
   }
 }
 
