68a9c82489ddbcebd968e42dd7ca9af40873c73a
samynebula <sam.zandiani@gmail.com>
2024-11-13 17:46:33 +0330

    Quartz sync: Nov 13, 2024, 5:46 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "سامان زندیانی",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
