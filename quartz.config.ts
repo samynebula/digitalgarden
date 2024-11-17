dd80ebf31db49484c10b45d9970f2521ae74d747
samynebula <sam.zandiani@gmail.com>
2024-11-17 13:45:16 +0330

    Quartz sync: Nov 17, 2024, 1:45 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -71,7 +71,7 @@ const config: QuartzConfig = {
       Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
       Plugin.Description(),
       Plugin.Latex({ renderEngine: "katex" }),
-      // Plugin.HardLineBreaks(),
+      Plugin.HardLineBreaks(),
     ],
     filters: [Plugin.RemoveDrafts()],
     emitters: [
