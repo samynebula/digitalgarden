8b2950af2836b7ef542be3621a8a6f81dec034bf
samynebula <sam.zandiani@gmail.com>
2024-11-17 15:20:38 +0330

    Quartz sync: Nov 17, 2024, 3:20 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -30,35 +30,39 @@ export const sharedPageComponents: SharedLayout = {
 
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
-  beforeBody: [
+    beforeBody: [
     Component.Breadcrumbs(),
     Component.ArticleTitle(),
     Component.ContentMeta(),
     Component.TagList(),
-  ],
-  left: [
-    Component.PageTitle(),
-    Component.MobileOnly(Component.Spacer()),
+    ],
+    left: [
+    Component.DesktopOnly(Component.Graph()),
+    Component.DesktopOnly(Component.Backlinks()),
+    Component.MobileOnly(Component.PageTitle()),
+    Component.MobileOnly(Component.Darkmode()),
+    ],
+    right: [
+    Component.DesktopOnly(Component.PageTitle()),
     Component.Search(),
-    Component.Darkmode(),
+    Component.DesktopOnly(Component.Darkmode()),
     Component.DesktopOnly(Component.Explorer()),
-  ],
-  right: [
-    Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
-    Component.Backlinks(),
-      ],
-}
-
-// components for pages that display lists of pages  (e.g. tags or folders)
-export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
-  left: [
-    Component.PageTitle(),
-    Component.MobileOnly(Component.Spacer()),
+    Component.MobileOnly(Component.Backlinks()),
+    ],
+  }
+   
+  // components for pages that display lists of pages  (e.g. tags or folders)
+  export const defaultListPageLayout: PageLayout = {
+   beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
+    left: [
+    Component.MobileOnly(Component.PageTitle()),
+    Component.MobileOnly(Component.Darkmode()),
+    ],
+    right: [
+    Component.DesktopOnly(Component.PageTitle()),
     Component.Search(),
-    Component.Darkmode(),
+    Component.DesktopOnly(Component.Darkmode()),
     Component.DesktopOnly(Component.Explorer()),
-  ],
-  right: [],
-}
+    ],
+  }
