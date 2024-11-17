66538ce5fa2a910c75c8906ecfe73f2e83e02f5a
samynebula <sam.zandiani@gmail.com>
2024-11-17 08:19:19 +0330

    Quartz sync: Nov 17, 2024, 8:19 AM

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -9,7 +9,7 @@ const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzCompo
   return (
     <h1 class={classNames(displayClass, "page-title")}>
       <div class="title-logo">
-        <a href={baseDir}><img id='icon-header' src={"https://blog.eledah.ir/static/icon-header.png"} alt="" /></a>
+        <a href={baseDir}><img id='icon-header' src={"https://samanz.ir/static/icon-header.png"} alt="" /></a>
         <a href={baseDir} class={displayClass} id="header-text">
           {title}
         </a>
